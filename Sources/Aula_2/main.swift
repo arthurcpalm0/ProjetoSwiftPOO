b11b4e78c6ce2f95c578de1a69ea33602ece4584
arthurcpalm0 <arthurcpalm0@gmail.com>
2026-04-28 20:38:23 +0000

    .

--- Sources/Aula_2/main.swift
+++ Sources/Aula_2/main.swift
@@ -1,10 +1,33 @@
 import Foundation
 
 protocol Manutencao{
-    var nomeItem: String
-    var historico: String
+    var nomeMaquina: String {get}
+    var historicoMaquina: String {get}
+    
+    func realizarManutencao(data: String) -> Bool
+    func estaFuncionando() -> Bool
 }
 
-class Equipamento{
+class Aparelho: Manutencao{
+    let nomeMaquina
+    private (set) var historicoMaquina: String = []
+    private (set) var estaFuncionando: Bool = true
 
+    init (nomeMaquina: String){
+        self.nomeMaquina = nomeMaquina
+    }
+
+    func realizarManutencao(data: String) -> Bool{
+        guard estaFuncionando else{
+            print ("A maquina \(nomeMaquina) está quebrada.")
+            return false
+        }
+        historicoMaquina.append(data)
+        print("Manutenção feita com sucesso")
+        return true
+    }
+
+    func estaFuncionando() -> Bool{
+        return historicoMaquina.isEmpty
+    }
 }
