c2e346bb8666039b1ccd0bbe6ed129dbe6a0e469
arthurcpalm0 <arthurcpalm0@gmail.com>
2026-04-29 20:40:32 +0000

    conteudo do dia 2 do projeto

---
+++ Sources/Aula_3/dia1.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+enum NivelExperiencia : String{
+    case iniciante = "iniciante"
+    case intermediario = "intermediario"
+    case avancado = "avançado"
+}
+
+enum AulasOferecidas : String{
+    case muscalacao = "musculação"
+    case spinning = "spinning"
+    case yoga = "yoga"
+    case luta = "luta"
+    case funcional = "funcional"
+}
+
+struct PlanosConfig{
+    let nome: String
+    let valormensal: Double
+    let personalTrainer: Bool
+    let limAulas: Int
+    let duracao: Int
+}
+
+enum CatalogoPlanos{
+    case mensal
+    case trimestral 
+    case anual
+
+    var atributos: PlanosConfig{
+        switch self{
+            case .mensal: return PlanosConfig(nome: "Mensal Simples", valormensal: 100, personalTrainer: false, limAulas: 10, duracao: 2)
+            case .trimestral: return PlanosConfig (nome: "Trimestral Premium", valormensal: 200, personalTrainer: true, limAulas: 20, duracao: 6)
+            case .anual: return PlanosConfig (nome: "Anual VIP", valormensal: 300, personalTrainer: true, limAulas: 30, duracao: 24)
+        }
+    }
+}
+
+class Pessoa{
+    let nome: String
+    let email: String
+    let funcao: String
+
+    init(nome: String, email: String, funcao: String){
+        self.nome = nome
+        self.email = email
+        self.funcao = funcao
+    }
+
+}
+
+class Aluno: Pessoa{
+    let matricula: String
+    var plano: CatalogoPlanos
+    var nivel: NivelExperiencia
+    init(nome: String, email: String, funcao: String, matricula: String, plano: CatalogoPlanos, nivel: NivelExperiencia){
+        self.matricula = matricula
+        self.plano = plano
+        self.nivel = nivel
+        super.init(nome: nome, email: email, funcao: "Aluno")
+    }
+}
+
+class Instrutor: Pessoa{
+    let especialidade: AulasOferecidas
+    init(nome: String, email: String, funcao: String, especialidade: AulasOferecidas){
+        self.especialidade = especialidade
+        super.init(nome: nome, email: email, funcao: "Instrutor")
+    }
+}
+
